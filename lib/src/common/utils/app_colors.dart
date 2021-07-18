d6cb8254433f0be2fe0d269efb6aa6816a9f2c51
amonyalife <amandos.zhanibek@mail.ru>
2021-07-18 13:00:00 +0600

    authEnd

---
+++ lib/src/common/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColor {
+  static const Color btnColor = Color.fromRGBO(70, 49, 210, 2);
+  static const Color scaffoldColor = Color(0xFFF3F4F6);
+  static const Color textFieldColor = Color(0xFFFEFEFE);
+}
--- lib/src/screens/auth.dart
+++ lib/src/screens/auth.dart
@@ -1,4 +1,8 @@
+import 'package:amonyatest/src/common/utils/app_colors.dart';
+import 'package:amonyatest/src/common/widgets/btn_button/btn_Button.dart';
+import 'package:amonyatest/src/common/widgets/textfield/txt_TextField.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class AuthScreen extends StatelessWidget {
   const AuthScreen({Key? key}) : super(key: key);
@@ -6,18 +10,28 @@ class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
+      backgroundColor: AppColor.scaffoldColor,
       navigationBar: CupertinoNavigationBar(
+        border: Border(),
+        backgroundColor: CupertinoColors.white,
         middle: Text("Авторизация"),
       ),
       child: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            CupertinoTextField(placeholder: "логин или почта"),
-            CupertinoTextField(placeholder: "пароль"),
-            CupertinoButton.filled(child: Text("Войти"), onPressed: () {}),
-            CupertinoButton.filled(
-                child: Text("Зарегистрироваться"), onPressed: () {})
+            TextfieldWidget(fieldName: "логин или почта"),
+            Container(
+              height: 1,
+              color: Color(0xFFE0E6ED),
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+            ),
+            TextfieldWidget(fieldName: "пароль"),
+            SizedBox(height: 32),
+            BtnButton(titleButton: "Войти", onPressed: () {}),
+            SizedBox(height: 19),
+            BtnButton(titleButton: "Зарегистрироваться", onPressed: () {})
           ],
         ),
       ),
